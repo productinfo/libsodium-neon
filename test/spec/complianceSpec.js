20dcb03f4ee078e6338773fbc1ebd67ec406eba7
Florian Keller <florian.keller@wire.com>
2017-03-08 12:12:36 +0100

    Add test for empty keys + Cleanup

--- test/spec/complianceSpec.js
+++ test/spec/complianceSpec.js
@@ -1,58 +1,72 @@
 'use strict';
 
-var libsodiumJS = require('libsodium-wrappers-sumo');
-var libsodiumNeon = require('../../lib');
+var helpers = require('../helpers');
+var libsodium = require('libsodium-wrappers-sumo');
+var libsodium_neon = require('../../lib');
 
-var message = 'Hello';
-var keyMaterial = new Uint8Array([5, 30, 208, 218, 140, 173, 89, 133, 238, 120, 243, 172, 56, 0, 84, 80, 225, 83, 110, 68, 59, 136, 105, 202, 200, 243, 73, 174, 28, 38, 66, 246]);
-var keyPairAlice, keyPairBob, curve25519SecretKeyAlice, curve25519SecretKeyBob, curve25519PublicKeyAlice, curve25519PublicKeyBob;
+var keypair_alice, keypair_bob, curve25519_secret_key_alice, curve25519_secret_key_bob, curve25519_public_key_bob;
+
+describe('libsodium-neon', function() {
+  it('assert_is_not_zeros', function() {
+    expect(helpers.assert_is_not_zeros([0, 0, 0, 0, 0])).toBe(false);
+    expect(helpers.assert_is_not_zeros([0, 0, 0, 230, 0])).toBe(true);
+    expect(helpers.assert_is_not_zeros([8, 51, 1, 103, 99])).toBe(true);
+    expect(helpers.assert_is_not_zeros(helpers.key_material)).toBe(true);
+  });
+
+  it('crypto_sign_keypair', function() {
+    keypair_alice = libsodium.crypto_sign_keypair();
+    expect(helpers.assert_is_not_zeros(keypair_alice.publicKey)).toBe(true);
+    expect(helpers.assert_is_not_zeros(keypair_alice.privateKey)).toBe(true);
+  });
+});
 
 describe('Compliance', function() {
   it('crypto_sign_keypair', function() {
-    keyPairAlice = libsodiumJS.crypto_sign_keypair();
-    keyPairBob = libsodiumNeon.crypto_sign_keypair();
+    keypair_alice = libsodium.crypto_sign_keypair();
+    keypair_bob = libsodium_neon.crypto_sign_keypair();
 
-    expect(keyPairBob.publicKey.length).toBeGreaterThan(0);
-    expect(keyPairAlice.publicKey.length).toEqual(keyPairBob.publicKey.length);
-    expect(keyPairAlice.privateKey.length).toEqual(keyPairBob.privateKey.length);
+    expect(keypair_bob.publicKey.length).toBeGreaterThan(0);
+    expect(keypair_alice.publicKey.length).toEqual(keypair_bob.publicKey.length);
+    expect(keypair_alice.privateKey.length).toEqual(keypair_bob.privateKey.length);
   });
 
   it('crypto_auth_hmacsha256', function() {
-    var auth = libsodiumJS.crypto_auth_hmacsha256(message, keyMaterial);
-    var authNeon = libsodiumNeon.crypto_auth_hmacsha256(message, keyMaterial);
+    var auth = libsodium.crypto_auth_hmacsha256(helpers.message, helpers.key_material);
+    var authNeon = libsodium_neon.crypto_auth_hmacsha256(helpers.message, helpers.key_material);
     expect(auth).toEqual(authNeon);
   });
 
   it('crypto_sign_detached', function() {
-    var sign = libsodiumJS.crypto_sign_detached(message, keyPairAlice.privateKey);
-    var signNeon = libsodiumNeon.crypto_sign_detached(message, keyPairAlice.privateKey);
+    var sign = libsodium.crypto_sign_detached(helpers.message, keypair_alice.privateKey);
+    var signNeon = libsodium_neon.crypto_sign_detached(helpers.message, keypair_alice.privateKey);
     expect(sign).toEqual(signNeon);
   });
 
   it('crypto_sign_ed25519_sk_to_curve25519', function() {
-    curve25519SecretKeyAlice = libsodiumJS.crypto_sign_ed25519_sk_to_curve25519(keyPairAlice.privateKey);
-    var curve25519SecretKeyAliceNeon = libsodiumJS.crypto_sign_ed25519_sk_to_curve25519(keyPairAlice.privateKey);
+    curve25519_secret_key_alice = libsodium.crypto_sign_ed25519_sk_to_curve25519(keypair_alice.privateKey);
+    var curve25519_secret_key_alice_neon = libsodium.crypto_sign_ed25519_sk_to_curve25519(keypair_alice.privateKey);
 
-    curve25519SecretKeyBob = libsodiumNeon.crypto_sign_ed25519_sk_to_curve25519(keyPairBob.privateKey);
-    var curve25519SecretKeyBobNeon = libsodiumNeon.crypto_sign_ed25519_sk_to_curve25519(keyPairBob.privateKey);
-    expect(curve25519SecretKeyAlice).toEqual(curve25519SecretKeyAliceNeon);
-    expect(curve25519SecretKeyBob).toEqual(curve25519SecretKeyBobNeon);
+    curve25519_secret_key_bob = libsodium_neon.crypto_sign_ed25519_sk_to_curve25519(keypair_bob.privateKey);
+    var curve25519_secret_key_bob_neon = libsodium_neon.crypto_sign_ed25519_sk_to_curve25519(keypair_bob.privateKey);
+    expect(curve25519_secret_key_alice).toEqual(curve25519_secret_key_alice_neon);
+    expect(curve25519_secret_key_bob).toEqual(curve25519_secret_key_bob_neon);
   });
 
   it('crypto_sign_ed25519_pk_to_curve25519', function() {
-    curve25519PublicKeyAlice = libsodiumJS.crypto_sign_ed25519_pk_to_curve25519(keyPairAlice.publicKey);
-    var curve25519PublicKeyAliceNeon = libsodiumJS.crypto_sign_ed25519_pk_to_curve25519(keyPairAlice.publicKey);
+    var curve25519_public_key_alice = libsodium.crypto_sign_ed25519_pk_to_curve25519(keypair_alice.publicKey);
+    var curve25519_public_key_alice_neon = libsodium.crypto_sign_ed25519_pk_to_curve25519(keypair_alice.publicKey);
 
-    curve25519PublicKeyBob = libsodiumNeon.crypto_sign_ed25519_pk_to_curve25519(keyPairBob.publicKey);
-    var curve25519PublicKeyBobNeon = libsodiumNeon.crypto_sign_ed25519_pk_to_curve25519(keyPairBob.publicKey);
+    curve25519_public_key_bob = libsodium_neon.crypto_sign_ed25519_pk_to_curve25519(keypair_bob.publicKey);
+    var curve25519_public_key_bob_neon = libsodium_neon.crypto_sign_ed25519_pk_to_curve25519(keypair_bob.publicKey);
 
-    expect(curve25519PublicKeyAlice).toEqual(curve25519PublicKeyAliceNeon);
-    expect(curve25519PublicKeyBob).toEqual(curve25519PublicKeyBobNeon);
+    expect(curve25519_public_key_alice).toEqual(curve25519_public_key_alice_neon);
+    expect(curve25519_public_key_bob).toEqual(curve25519_public_key_bob_neon);
   });
 
   it('crypto_scalarmult', function() {
-    var scalar = libsodiumJS.crypto_scalarmult(curve25519SecretKeyAlice, curve25519PublicKeyBob);
-    var scalarNeon = libsodiumNeon.crypto_scalarmult(curve25519SecretKeyAlice, curve25519PublicKeyBob);
-    expect(scalar).toEqual(scalarNeon);
+    var scalar = libsodium.crypto_scalarmult(curve25519_secret_key_alice, curve25519_public_key_bob);
+    var scalar_neon = libsodium_neon.crypto_scalarmult(curve25519_secret_key_alice, curve25519_public_key_bob);
+    expect(scalar).toEqual(scalar_neon);
   });
 });
